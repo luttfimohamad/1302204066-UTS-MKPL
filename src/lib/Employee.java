208791c8a764fb6510d58d8562132b82e560f775
luttfimohamad <luttfimohamad@gmail.com>
2023-04-15 14:18:14 +0700

    membuat class EmployeeSalaryGrade

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -10,8 +10,7 @@ public class Employee {
 	private EmployeePersonalInformation employeePersonalInformation;
 
 	// EmployeeGrade
-	private int monthlySalary;
-	private boolean isForeigner;
+	private EmployeeSalaryGrade employeeSalaryGrade;
 
 	// EmployeeAdditionalIncome
 	private int otherMonthlyIncome;
@@ -29,36 +28,10 @@ public class Employee {
 			LocalDate joinDate, boolean isForeigner, EmployeePersonalInformation.Gender gender) {
 		this.employeePersonalInformation = new EmployeePersonalInformation(employeeId, firstName, lastName, idNumber,
 				address, joinDate, gender);
-		this.isForeigner = isForeigner;
+		this.employeeSalaryGrade = new EmployeeSalaryGrade();
 
 		childNames = new LinkedList<String>();
 		childIdNumbers = new LinkedList<String>();
-	}
-
-	/**
-	 * Fungsi untuk menentukan gaji bulanan pegawai berdasarkan grade kepegawaiannya
-	 * (grade 1: 3.000.000 per bulan, grade 2: 5.000.000 per bulan, grade 3:
-	 * 7.000.000 per bulan)
-	 * Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
-	 */
-
-	public void setMonthlySalary(int grade) {
-		if (grade == 1) {
-			monthlySalary = 3000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		} else if (grade == 2) {
-			monthlySalary = 5000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		} else if (grade == 3) {
-			monthlySalary = 7000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}
 	}
 
 	public void setAnnualDeductible(int deductible) {
